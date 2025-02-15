1832b5efee517464907ea7a3206304e3ace6be23
PRAVEEN KANNA K R <praveenkannakr@PRAVEENS-MAC.local>
2025-02-15 09:23:39 +0530

    Added react

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h1>Praveen kanna</h1>
+            <p>Contact: krpraveenkanna06@gmail.com</p>
+            <p>Phone: +91 7904729229</p>
+        </div>
+    );
+};
+
+export default Home;
